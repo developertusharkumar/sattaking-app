6e36a5190ee95b9a73dad7f5c04f99bd172fde88
Aryansharma279 <official.aryansharma01@gmail.com>
2021-10-23 18:24:13 +0530

    [NEW] For Testing on App

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAxeXoh1ugfL4LWR3SOhiucsQcRGqyGFTM",
+    authDomain: "testing-sattaking.firebaseapp.com",
+    databaseURL: "https://testing-sattaking-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "testing-sattaking",
+    storageBucket: "testing-sattaking.appspot.com",
+    messagingSenderId: "765939966493",
+    appId: "1:765939966493:web:3fcc353baf13654d369cf4",
+    measurementId: "G-6WGF04YDSR"
+  },
 };
